3596bb1a196832b35d3e79bf358ea443e9f5e1c7
Laurent B <laurentbernard.pro@gmail.com>
2021-09-18 16:35:49 +0200

    suite

--- BlazorWjdr.DataSource/JsonDto/JsonDieu.cs
+++ BlazorWjdr.DataSource/JsonDto/JsonDieu.cs
@@ -1,5 +1,6 @@
 ﻿// ReSharper disable InconsistentNaming
 // ReSharper disable ClassNeverInstantiated.Global
+// ReSharper disable UnusedAutoPropertyAccessor.Global
 namespace BlazorWjdr.DataSource.JsonDto
 {
     using System.Collections.Generic;
@@ -37,32 +38,33 @@ namespace BlazorWjdr.DataSource.JsonDto
     public class JsonDieu
     {
         public int id { get; set; }
-        public int? patron { get; set; }
-        public string nom { get; set; }
-        public string domaines { get; set; }
-        public string fideles { get; set; }
-        public string symboles { get; set; }
-        public string symboles_image { get; set; }
-        public string histoire { get; set; }
-        public string comment { get; set; }
+        public int? patron { get; set; } = null;
+        public string nom { get; set; } = null!;
+        
+        public string? domaines { get; set; }
+        public string? fideles { get; set; }
+        public string? histoire { get; set; }
+        public string? comment { get; set; }
+        
+        public string? symboles { get; set; }
         public int? siege { get; set; }
-        public JsonAptitudesAssociees JsonAptitudesAssociees { get; set; }
-        public string chef { get; set; }
-        public string fetes { get; set; }
-        public string livres { get; set; }
-        public string intro { get; set; }
-        public string culte { get; set; }
-        public List<JsonRegleAssociee> regles { get; set; }
-        public string dogme { get; set; }
-        public string initiation { get; set; }
-        public string pretrise { get; set; }
-        public List<string> commandements { get; set; }
-        public string cultistes { get; set; }
-        public string structure { get; set; }
-        public List<JsonSecte> sectes { get; set; }
-        public List<string> ambiance { get; set; }
-        public List<JsonTemple> temples { get; set; }
-        public List<JsonPersonnalite> personnalites { get; set; }
+        public JsonAptitudesAssociees? JsonAptitudesAssociees { get; set; }
+        public string? chef { get; set; }
+        public string? fetes { get; set; }
+        public string? livres { get; set; }
+        public string? intro { get; set; }
+        public string? culte { get; set; }
+        public JsonRegleAssociee[]? regles { get; set; }
+        public string? dogme { get; set; }
+        public string? initiation { get; set; }
+        public string? pretrise { get; set; }
+        public string[]? commandements { get; set; }
+        public string? cultistes { get; set; }
+        public string? structure { get; set; }
+        public JsonSecte[]? sectes { get; set; }
+        public string[]? ambiance { get; set; }
+        public JsonTemple[]? temples { get; set; }
+        public JsonPersonnalite[]? personnalites { get; set; }
     }
 
     public class RootDieu
