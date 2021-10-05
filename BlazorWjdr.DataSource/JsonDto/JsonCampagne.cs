e7a18c69d5932ce662d9bd3e609243a2d88651f1
Laurent B <laurentbernard.pro@gmail.com>
2021-10-06 01:00:38 +0200

    prepa campagne

---
+++ BlazorWjdr.DataSource/JsonDto/JsonCampagne.cs
@@ -0,0 +1,57 @@
+﻿// ReSharper disable InconsistentNaming
+// ReSharper disable ClassNeverInstantiated.Global
+namespace BlazorWjdr.DataSource.JsonDto
+{
+    using System.Collections.Generic;
+
+    public class JsonUser
+    {
+        public int id { get; set; }
+        public string email { get; set; } = null!;
+        public string pseudo { get; set; } = null!;
+    }
+
+    public class JsonCampagne
+    {
+        public int id { get; set; }
+        public string titre { get; set; } = null!;
+        public int mj { get; set; }
+    }
+
+    public class JsonTeam
+    {
+        public int id { get; set; }
+        public string nom { get; set; } = null!;
+    }
+
+    public class JsonFact
+    {
+        public int tri { get; set; }
+        public int[]? pjs { get; set; }
+        public string fact { get; set; } = null!;
+    }
+
+    public class JsonSeance
+    {
+        public int id { get; set; }
+        public int team { get; set; }
+        public int campagne { get; set; }
+        public string quand { get; set; } = null!;
+        public int acte { get; set; }
+        public int duree { get; set; }
+        public string titre { get; set; } = null!;
+        public int xp { get; set; }
+        public string? xp_comment { get; set; }
+        public string? resume { get; set; }
+        public int[]? pjs { get; set; }
+        public JsonFact[]? facts { get; set; }
+    }
+
+    public class RootCampagne
+    {
+        public List<JsonUser> users { get; set; } = null!;
+        public List<JsonCampagne> campagnes { get; set; } = null!;
+        public List<JsonTeam> teams { get; set; } = null!;
+        public List<JsonSeance> items { get; set; } = null!;
+    }
+}
---
+++ BlazorWjdr.Models/CampagneDto.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+
+namespace BlazorWjdr.Models
+{
+    public class RootCampagneDto
+    {
+        private readonly Dictionary<int, UserDto> _users;
+        private readonly Dictionary<int, CampagneDto> _campagnes;
+        private readonly Dictionary<int, TeamDto> _teams;
+    }
+    
+    public class UserDto
+    {
+        public int Id { get; set; }
+        public string Email { get; set; } = null!;
+        public string Pseudo { get; set; } = null!;
+    }
+
+    public class CampagneDto
+    {
+        public int Id { get; set; }
+        public string Titre { get; set; } = null!;
+        public int Mj { get; set; }
+    }
+
+    public class TeamDto
+    {
+        public int Id { get; set; }
+        public string Nom { get; set; } = null!;
+    }
+
+    public class FactDto
+    {
+        public int Tri { get; set; }
+        public UserDto[] Pjs { get; set; }
+        public string Fact { get; set; } = null!;
+    }
+
+    public class SeanceDto
+    {
+        public TeamDto? Team { get; set; }
+        public CampagneDto Campagne { get; set; }
+        public string Quand { get; set; } = null!;
+        public int Acte { get; set; }
+        public int Duree { get; set; }
+        public string Titre { get; set; } = null!;
+        public int Xp { get; set; }
+        public string XpComment { get; set; }
+        public string Resume { get; set; }
+        public UserDto[] Pjs { get; set; }
+        public FactDto[]? Facts { get; set; }
+    }
+}
