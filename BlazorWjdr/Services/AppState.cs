5ce39c519ce29832c5ef587dd50fd8de1569860c
Laurent B <lbernard@formiris.com>
2021-11-26 21:53:50 +0100

    vlan

--- BlazorWjdr/Services/AppState.cs
+++ BlazorWjdr/Services/AppState.cs
@@ -5,19 +5,19 @@ namespace BlazorWjdr.Services
     public class AppState
     {
         public bool JeSuisDieu { get; private set; }
-
         public event Action? OnChange;
+        
+        private void NotifyStateChanged() => OnChange?.Invoke();
 
         public void UnCombattantEnEngageUnAutre()
         {
             NotifyStateChanged();
         }
 
-        private void NotifyStateChanged() => OnChange?.Invoke();
-
         public void PretendreEtreUnDieu(string password)
         {
             JeSuisDieu = GenericService.DieuEstDAccord(password);
+            NotifyStateChanged();
         }
     }
 }
