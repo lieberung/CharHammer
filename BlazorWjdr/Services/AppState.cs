37d0adc826f1f42573941a5a1a0c1156e7598537
laurentb <laurentbernard.pro@gmail.com>
2023-11-29 23:07:57 +0100

    refonte MainTopRow

--- BlazorWjdr/Services/AppState.cs
+++ BlazorWjdr/Services/AppState.cs
@@ -4,10 +4,24 @@ namespace BlazorWjdr.Services;
 
 public class AppState
 {
+    public string MainTopRowTitle { get; private set; } = "Charente Hammer";
+    public string PageHeadTitle { get; private set; } = "Charente Hammer";
     public bool JeSuisDieu { get; private set; }
     public event Action? OnChange;
     
     private void NotifyStateChanged() => OnChange?.Invoke();
+
+    public void SetPageHeadTitle(string title)
+    {
+        PageHeadTitle = title;
+        NotifyStateChanged();
+    }
+
+    public void SetMainTopRowTitle(string title)
+    {
+        MainTopRowTitle = title;
+        NotifyStateChanged();
+    }
 
     public void UnCombattantEnEngageUnAutre()
     {
